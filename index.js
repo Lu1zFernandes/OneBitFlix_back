6af3c95b82971517607de5a1d6fe488edb1d43cd
Luiz Fernandes <luiz.fernandes31matos@gmail.com>
2023-09-29 09:38:14 -0300

    Iniciando o Projeto e Configurando o Sequelize

--- index.js
+++
@@ -1,8 +0,0 @@
-const hello = "Hello world!";
-console.log(hello);
-
-function sum(a, b) {
-  return a + b;
-}
-
-console.log(sum(2, 2));
---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import { database } from "./database";
+
+const app = express();
+
+const PORT = process.env.port || 3000;
+
+app.listen(PORT, () => {
+  database.authenticate().then(() => {
+    console.log("DB connection successfull.");
+  });
+
+  console.log(`Server started successfuly at port ${PORT}.`);
+});
