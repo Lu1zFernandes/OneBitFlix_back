3065a751c27af965c19f3cc8c4c0d0f386f04af6
Luiz Fernandes <luiz.fernandes31matos@gmail.com>
2023-10-02 16:11:45 -0300

    Adicionando traduções

--- src/adminjs/index.ts
+++ src/adminjs/index.ts
@@ -5,6 +5,7 @@ import { sequelize } from "../database";
 import { adminJsResources } from "./resources";
 import { User } from "../models";
 import bcrypt from "bcrypt";
+import { locale } from "./locale";
 
 AdminJs.registerAdapter(AdminJsSequelize);
 
@@ -12,6 +13,7 @@ export const adminJs = new AdminJs({
   databases: [sequelize],
   resources: adminJsResources,
   rootPath: "/admin",
+  locale: locale,
   branding: {
     companyName: "OneBitFlix",
     logo: "/onebitflix.svg",
