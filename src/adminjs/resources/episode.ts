dc8328b952e7ba444d58d681e7504725ce82f705
Luiz Fernandes <luiz.fernandes31matos@gmail.com>
2023-10-02 09:16:58 -0300

    Upload de arquivos no Adminjs e tentando consertar um erro

--- src/adminjs/resources/episode.ts
+++ src/adminjs/resources/episode.ts
@@ -1,4 +1,6 @@
-import { ResourceOptions } from "adminjs";
+import path from "path";
+import uploadFileFeature from "@adminjs/upload";
+import { FeatureType, ResourceOptions } from "adminjs";
 
 export const episodeResourceOptions: ResourceOptions = {
   navigation: "Catálogo",
@@ -31,3 +33,19 @@ export const episodeResourceOptions: ResourceOptions = {
     "updatedAt",
   ],
 };
+
+export const episodeResourceFeatures: FeatureType[] = [
+  uploadFileFeature({
+    provider: {
+      local: {
+        bucket: path.join(__dirname, "..", "..", "..", "uploads"),
+      },
+    },
+    properties: {
+      key: "videoUrl",
+      file: "uploadVideo",
+    },
+    uploadPath: (record, filename) =>
+      `videos/course-${record.get("courseId")}/${filename}`,
+  }),
+];
--- src/adminjs/resources/index.ts
+++ src/adminjs/resources/index.ts
@@ -2,7 +2,7 @@ import { ResourceWithOptions } from "adminjs";
 import { Category, Course, Episode } from "../../models";
 import { categoryResourceOptions } from "./category";
 import { courseResourceOptions } from "./course";
-import { episodeResourceOptions } from "./episode";
+import { episodeResourceFeatures, episodeResourceOptions } from "./episode";
 
 export const adminJsResources: ResourceWithOptions[] = [
   {
@@ -16,5 +16,6 @@ export const adminJsResources: ResourceWithOptions[] = [
   {
     resource: Episode,
     options: episodeResourceOptions,
+    features: episodeResourceFeatures,
   },
 ];
