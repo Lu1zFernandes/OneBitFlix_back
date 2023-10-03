9c16a6f7af8217c22591e949a466c1b63e020ab8
Luiz Fernandes <luiz.fernandes31matos@gmail.com>
2023-10-03 14:53:10 -0300

    Refatorando as configurações do Adminjs

--- src/adminjs/index.ts
+++ src/adminjs/index.ts
@@ -6,6 +6,9 @@ import { adminJsResources } from "./resources";
 import { Category, Course, Episode, User } from "../models";
 import bcrypt from "bcrypt";
 import { locale } from "./locale";
+import { authtenticationOptions } from "./authentication";
+import { dashboardOptions } from "./dashboard";
+import { brandingOptions } from "./branding";
 
 AdminJs.registerAdapter(AdminJsSequelize);
 
@@ -14,64 +17,13 @@ export const adminJs = new AdminJs({
   resources: adminJsResources,
   rootPath: "/admin",
   locale: locale,
-  dashboard: {
-    component: AdminJs.bundle("./components/Dashboard"),
-    handler: async (req, res, context) => {
-      const courses = await Course.count();
-      const episodes = await Episode.count();
-      const category = await Category.count();
-      const standardUsers = await User.count({ where: { role: "user" } });
-
-      res.json({
-        Cursos: courses,
-        Episódios: episodes,
-        Categorias: category,
-        Usuários: standardUsers,
-      });
-    },
-  },
-  branding: {
-    companyName: "OneBitFlix",
-    logo: "/onebitflix.svg",
-    theme: {
-      colors: {
-        primary100: "#ff0043",
-        primary80: "#ff1a57",
-        primary60: "#ff3369",
-        primary40: "#ff4d7c",
-        primary20: "#ff668f",
-        grey100: "#151515",
-        grey80: "#333333",
-        grey60: "#4d4d4d",
-        grey40: "#666666",
-        grey20: "#dddddd",
-        filterBg: "#333333",
-        accent: "#151515",
-        hoverBg: "#151515",
-      },
-    },
-  },
+  dashboard: dashboardOptions,
+  branding: brandingOptions,
 });
-//export const adminJsRouter = AdminJsExpress.buildRouter(adminJs);
 
 export const adminJsRouter = AdminJsExpress.buildAuthenticatedRouter(
   adminJs,
-  {
-    authenticate: async (email, password) => {
-      const user = await User.findOne({ where: { email } });
-
-      if (user && user.role === "admin") {
-        const matched = await bcrypt.compare(password, user.password);
-
-        if (matched) {
-          return user;
-        }
-      }
-
-      return false;
-    },
-    cookiePassword: "senha-do-cookie",
-  },
+  authtenticationOptions,
   null,
   {
     resave: false,
