829c7291b9b27793006bbb9f5701d85e198d8db7
Luiz Fernandes <luiz.fernandes31matos@gmail.com>
2023-10-04 10:13:19 -0300

    Obtendo 3 cursos em destaque

--- src/services/courseService.ts
+++ src/services/courseService.ts
@@ -21,4 +21,19 @@ export const courseService = {
 
     return courseWithEpisodes;
   },
+
+  getRandomFeaturedCourses: async () => {
+    const featuredCourses = await Course.findAll({
+      attributes: ["id", "name", "synopsis", ["thumbnail_url", "thumbnailUrl"]],
+      where: {
+        featured: true,
+      },
+    });
+
+    const randomFeaturedCourses = featuredCourses.sort(
+      () => 0.5 - Math.random()
+    );
+
+    return randomFeaturedCourses.slice(0, 3);
+  },
 };
