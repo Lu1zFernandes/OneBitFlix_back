ae62441dd62f1e4ce2288ce0ea7b219a46c8a795
Luiz Fernandes <luiz.fernandes31matos@gmail.com>
2023-10-04 15:31:27 -0300

    Streaming de Vídeo na API

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { categoriesController } from "./controllers/categoriesController";
 import { coursesController } from "./controllers/courseController";
+import { episodesController } from "./controllers/episodesController";
 
 const router = express.Router();
 
@@ -12,4 +13,5 @@ router.get("/courses/newest", coursesController.newest);
 router.get("/courses/search", coursesController.search);
 router.get("/courses/:id", coursesController.show);
 
+router.get("/episodes/stream", episodesController.stream);
 export { router };
