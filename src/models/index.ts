42a8a0e82569cc6d24305b4ad34a43f6730e4f00
Luiz Fernandes <luiz.fernandes31matos@gmail.com>
2023-10-05 15:16:54 -0300

    Adicionando favoritos

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,12 +1,22 @@
 import { Category } from "./Category";
 import { Course } from "./Course";
 import { Episode } from "./Episode";
+import { Favorite } from "./Favorite";
 import { User } from "./User";
 
 Category.hasMany(Course, { as: "courses" });
 Course.belongsTo(Category);
 
+Course.belongsToMany(User, { through: Favorite });
+Course.hasMany(Favorite, { as: "favoritesUsers", foreignKey: "course_id" });
+
 Course.hasMany(Episode, { as: "episodes" }); //Episodes
 Episode.belongsTo(Course);
 
-export { Course, Category, Episode, User };
+Favorite.belongsTo(Course);
+Favorite.belongsTo(User);
+
+User.belongsToMany(Course, { through: Favorite });
+User.hasMany(Favorite, { as: "favoritesCourses", foreignKey: "user_id" });
+
+export { Course, Category, Episode, Favorite, User };
