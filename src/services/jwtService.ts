46fa999939b4c47b8256cc7e54411e6c8b427918
Luiz Fernandes <luiz.fernandes31matos@gmail.com>
2023-10-05 11:31:10 -0300

    Middleware de autorização

--- src/services/jwtService.ts
+++ src/services/jwtService.ts
@@ -6,4 +6,8 @@ export const jwtService = {
   signToken: (payload: string | object | Buffer, expiration: string) => {
     return jwt.sign(payload, secret, { expiresIn: expiration });
   },
+
+  verifyToken: (token: string, callbackfn: jwt.VerifyCallback) => {
+    jwt.verify(token, secret, callbackfn);
+  },
 };
