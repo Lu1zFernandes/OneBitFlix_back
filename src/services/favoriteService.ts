8e199b0cd3bf064906a8535821d879dd061a3564
Luiz Fernandes <luiz.fernandes31matos@gmail.com>
2023-10-06 09:32:10 -0300

    Atualizando o endpoint de cursos

--- src/services/favoriteService.ts
+++ src/services/favoriteService.ts
@@ -38,4 +38,15 @@ export const favoriteService = {
       },
     });
   },
+
+  isFavorited: async (userId: number, courseId: number) => {
+    const favorite = await Favorite.findOne({
+      where: {
+        userId,
+        courseId,
+      },
+    });
+
+    return favorite !== null ? true : false;
+  },
 };
