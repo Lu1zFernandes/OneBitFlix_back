c100168a8f49bb202821b2f717cbea13dd897720
Luiz Fernandes <luiz.fernandes31matos@gmail.com>
2023-10-06 10:55:53 -0300

    Adicionando o progresso de um episodio

--- src/models/index.ts
+++ src/models/index.ts
@@ -4,6 +4,7 @@ import { Episode } from "./Episode";
 import { Favorite } from "./Favorite";
 import { Like } from "./Like";
 import { User } from "./User";
+import { WatchTime } from "./WatchTime";
 
 Category.hasMany(Course, { as: "courses" });
 Course.belongsTo(Category);
@@ -14,12 +15,14 @@ Course.hasMany(Favorite, { as: "favoritesUsers", foreignKey: "course_id" });
 
 Course.hasMany(Episode, { as: "episodes" }); //Episodes
 Episode.belongsTo(Course);
+Episode.belongsToMany(User, { through: WatchTime });
 
 Favorite.belongsTo(Course);
 Favorite.belongsTo(User);
 
 User.belongsToMany(Course, { through: Favorite });
 User.belongsToMany(Course, { through: Like });
+User.belongsToMany(Episode, { through: WatchTime });
 User.hasMany(Favorite, { as: "favoritesCourses", foreignKey: "user_id" });
 
-export { Course, Category, Episode, Favorite, Like, User };
+export { Course, Category, Episode, Favorite, Like, User, WatchTime };
