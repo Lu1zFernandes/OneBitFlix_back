79841dea274cb0885c8f1b0946df1654f5b3ef28
Luiz Fernandes <luiz.fernandes31matos@gmail.com>
2023-10-06 08:51:21 -0300

    Criando o gostei

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,12 +2,14 @@ import { Category } from "./Category";
 import { Course } from "./Course";
 import { Episode } from "./Episode";
 import { Favorite } from "./Favorite";
+import { Like } from "./Like";
 import { User } from "./User";
 
 Category.hasMany(Course, { as: "courses" });
 Course.belongsTo(Category);
 
 Course.belongsToMany(User, { through: Favorite });
+Course.belongsToMany(User, { through: Like });
 Course.hasMany(Favorite, { as: "favoritesUsers", foreignKey: "course_id" });
 
 Course.hasMany(Episode, { as: "episodes" }); //Episodes
@@ -17,6 +19,7 @@ Favorite.belongsTo(Course);
 Favorite.belongsTo(User);
 
 User.belongsToMany(Course, { through: Favorite });
+User.belongsToMany(Course, { through: Like });
 User.hasMany(Favorite, { as: "favoritesCourses", foreignKey: "user_id" });
 
-export { Course, Category, Episode, Favorite, User };
+export { Course, Category, Episode, Favorite, Like, User };
